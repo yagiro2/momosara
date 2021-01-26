4f23d05d8f9334bf4434769d116fea9a614fd1db
Jagiro Elmodovar <jagiro@walla.com>
2021-01-26 16:25:44 +0200

    add start-dev bash script

---
+++ start-dev.sh
@@ -0,0 +1,5 @@
+# "&" - background process
+
+code . & # open the project in VSCode 
+open -a "Google Chrome" https://momosara.com & # pretty obvious
+theme watch --allow-live # start file watcher (deploys changes to LIVE theme on shopify!)
